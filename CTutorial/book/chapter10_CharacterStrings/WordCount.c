1850036c42e6710d2d7808dc487453559800aad9
BabitzdaBrave <amg.vintila@gmail.com>
2015-12-28 04:15:22 -0800

    Wordcount done

--- CTutorial/book/chapter10_CharacterStrings/WordCount.c
+++ CTutorial/book/chapter10_CharacterStrings/WordCount.c
@@ -3,6 +3,22 @@
 
 
 
+void readLine(char buffer[]) {
+
+    char character;
+    int i = 0;
+
+    do {
+        character = getchar();
+        buffer[i] = character;
+        ++i;
+    } while (character != '\n');
+
+    buffer[i-1] = '\0';
+}
+
+
+
 bool isAlphabetic(const char c) {
 
     return ((c >= 'a' && c <= 'z')
@@ -16,5 +32,31 @@ int countWords (const char string[]){
     int wordCount = 0;
     bool lookingForWord = true;
 
+    for (int i = 0; string[i] != '\0'; i++)
+    {
+        if (isAlphabetic(string[i]))
+        {
+            if (lookingForWord){
+                wordCount++;
+                lookingForWord = false;
+            }
+        } else {
+            lookingForWord = true;
+        }
+    }
+    return wordCount;
+}
 
+
+
+int main() {
+
+    char input[81];
+
+    printf("Enter a text:\n");
+    readLine(input);
+
+    printf("Number of words: %i", countWords(input));
+
+    return 0;
 }
