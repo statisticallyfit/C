5249d33349b70a841b38999a7d8f0c7fdada6da8
BabitzdaBrave <amg.vintila@gmail.com>
2015-12-29 08:29:51 -0800

    going to do exercise 4 substring

--- CTutorial/bookKochan/chapter10_CharacterStrings/WordCountLine.c
+++ CTutorial/bookKochan/chapter10_CharacterStrings/WordCountLine.c
@@ -26,20 +26,26 @@ bool isAlphabetic(const char c) {
 }
 
 
-
-int countLineWords(const char *const string){
+/**
+ * Exercise 3: fixing function to not count a new word
+ * after a word that contains an apostrophe.
+ * If there is space around the apostrophe, the (letter) after apostrophe will count as new word
+ */
+int countLineWords(const char line[]){
 
     int wordCount = 0;
     bool lookingForWord = true;
 
-    for (int i = 0; string[i] != '\0'; i++)
+    for (int i = 0; line[i] != '\0'; i++)
     {
-        if (isAlphabetic(string[i]))
+        if (isAlphabetic(line[i]))
         {
             if (lookingForWord){
                 wordCount++;
                 lookingForWord = false;
             }
+        } else if (line[i] == '\''){ // if apostrophe
+            lookingForWord = false;
         } else {
             lookingForWord = true;
         }
@@ -53,7 +59,7 @@ int main() {
 
     char input[81];
 
-    printf("Enter a text:\n");
+    printf("Enter a line:\n");
     readLine(input);
 
     printf("Number of words: %i", countLineWords(input));
