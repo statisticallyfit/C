f147c8b5ccd1cfc023fc176a609288f783e87e54
BabitzdaBrave <amg.vintila@gmail.com>
2016-01-04 07:54:02 -0800

    page 274 of C kochan book and traversed linkedlist with while loop

--- CTutorial/c/bookKochan/chapter11_Pointers/LinkedListPointer.c
+++ CTutorial/c/bookKochan/chapter11_Pointers/LinkedListPointer.c
@@ -10,15 +10,30 @@ int main () {
 
     struct Entry n1, n2, n3;
 
-    n1.value = 100;
-    n2.value = 200;
-    n3.value = 300;
+    struct Entry *listPointer;
+    listPointer = &n1;
 
+    n1.value = 100;
     n1.next = &n2;
+
+    n2.value = 200;
     n2.next = &n3;
 
-    printf("\n%i\n", n1.next->value);
-    printf("%i\n", n2.next->value);
+    n3.value = 300;
+    n3.next = (struct Entry *) 0; //marking end of list with null pointer
+
+    /*printf("\nlistptr value: %i\n", (*listPointer).value);
+    printf("listptr nextval: %i\n", (*listPointer).next->value);
+    printf("%i\n", n1.next->value); // means (*(n1.next)).value
+    printf("%i\n", n2.next->value);*/
+
+    // Traversing linkedlist
+    struct Entry *nullPointer = (struct Entry *) 0;
+    while (listPointer != nullPointer)
+    {
+        printf("%i\n", listPointer->value);
+        listPointer = listPointer->next;
+    }
 
     return 0;
 }
