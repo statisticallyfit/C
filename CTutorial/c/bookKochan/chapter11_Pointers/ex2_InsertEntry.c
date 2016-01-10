25e68fa2ff18ede1dd0475b10ac62f2bea3b03c2
BabitzdaBrave <amg.vintila@gmail.com>
2016-01-10 03:44:07 -0800

    don't know how to make linkedlist insertion

---
+++ CTutorial/c/bookKochan/chapter11_Pointers/ex2_InsertEntry.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+
+
+
+struct Entry {
+    int value;
+    struct Entry *next;
+};
+
+
+
+void insertEntry(struct Entry insert, struct Entry after)
+{
+    insert.next = after.next;
+    after.next = &insert;
+    //printf("After entry:", after.value);
+    /*(*insert).next = (*after).next; //same as after->next
+    (*after).next = &insert;*/
+}
+
+
+void traverseLinkedList(struct Entry *listPointer)
+{
+    struct Entry *nullPointer = (struct Entry *) 0;
+    while (listPointer != nullPointer)
+    {
+        printf("%i\n", listPointer->value);
+        listPointer = listPointer->next;
+    }
+}
+
+
+
+int main() {
+
+
+    struct Entry e1, e2, e4, e5;
+    struct Entry *listPointer;
+
+    listPointer = &e1;
+
+    e1.value = 10;
+    e1.next = &e2;
+
+    e2.value = 20;
+    e2.next = &e4;
+
+    e4.value = 40;
+    e4.next = &e5;
+
+    e5.value = 50;
+    e5.next = (struct Entry *) 0;
+
+
+    //insert entry
+    struct Entry entryInsert;
+    entryInsert.value = 30;
+    entryInsert.next = (struct Entry *) 0;
+
+    printf("\nBefore inserting:\n");
+    traverseLinkedList(listPointer);
+    printf("After inserting:\n");
+    insertEntry(entryInsert, e2);
+    traverseLinkedList(listPointer);
+
+    return 0;
+}
